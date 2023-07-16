7906d78144fa3988ae11178219e9235718a1e9af
Saurabh Rana <saurabhrana200317@gmail.com>
2023-07-17 00:38:01 +0530

    step 7

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 var buttonColours = ["red", "blue", "green", "yellow"]
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;  
+var started = false;
 function nextSequence() {
     var randomNumber = Math.random() * 4;
     randomNumber = Math.floor(randomNumber);
@@ -9,10 +11,14 @@ function nextSequence() {
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     
     playSound(randomChosenColour);
+    level++;
+    $("h1").text("level " + level);
+
+
 
 }
 $(".btn").click(function () {
-    userChosenColour = this.id;
+    var userChosenColour = this.id;
     userClickedPattern.push(userChosenColour);
     // console.log(userClickedPattern);
     playSound(userChosenColour);
@@ -20,10 +26,22 @@ $(".btn").click(function () {
 function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
+    
 }
 function animatePress(currentColour) {
     $("." + currentColour).addClass("pressed");
     setTimeout(function () {
-        $("." + currentColour).removeClass("pressed");
-    }, 100);
+    $("." + currentColour).removeClass("pressed");
+     }, 100);
+    started = "true"
 }
+
+$(document).keypress(function () {
+    if (!started) {
+        nextSequence();
+        $("h1").text("level " + level);
+        started = true;
+    }
+});
+
+

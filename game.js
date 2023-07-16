a5057e1dec2c9fe22eb9fc82a6d27f03adddcc2f
Saurabh Rana <saurabhrana200317@gmail.com>
2023-07-16 23:16:52 +0530

    jquery

---
+++ game.js
@@ -0,0 +1,8 @@
+var buttonColours = ["red", "blue", "green", "yellow"]
+var gamePattern = [];
+function nextSequence() {
+    var randomNumber = Math.random() * 4;
+    randomNumber = Math.floor(randomNumber);
+    var randomChosenColour = buttonColours[randomNumber];
+    buttonColours.push(randomChosenColour);
+}

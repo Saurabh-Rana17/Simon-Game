fa0fe8a605e45915e7b16f6aea4a49c7b2d176ed
Saurabh Rana <saurabhrana200317@gmail.com>
2023-07-17 00:07:03 +0530

    sound added

--- game.js
+++ game.js
@@ -1,9 +1,26 @@
 var buttonColours = ["red", "blue", "green", "yellow"]
 var gamePattern = [];
+var userClickedPattern = [];
 function nextSequence() {
     var randomNumber = Math.random() * 4;
     randomNumber = Math.floor(randomNumber);
     var randomChosenColour = buttonColours[randomNumber];
     buttonColours.push(randomChosenColour);
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+    playSound(randomChosenColour);
+
+}
+$(".btn").click(function () {
+    userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+    playSound(userChosenColour);
+});
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+function animatePress(currentColour) {
+    
 }
